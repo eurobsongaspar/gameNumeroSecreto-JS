8bb37963d731b7f4d04f8c13405701042bbe2006
Robson Gaspar <orobsongaspar@gmail.com>
2024-10-04 04:46:36 -0300

    Fiz algumas correções

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let nome = prompt('Olá, seja bem-vindo ao jogo de adivinhar números. Para condinuar, digite o seu nome!');
+let nome = prompt('Olá, seja bem-vindo ao jogo de adivinhar números. Para continuar, digite o seu nome!');
 let numeroSecreto = Math.floor(Math.random() * 5) + 1; // Cria um número rondômico entre 1 e 10
 console.log(numeroSecreto);
 let chute;
@@ -8,7 +8,7 @@ while (chute != numeroSecreto) {
    chute = prompt (`Ok, ${nome}. Escolha um número entre 1 e 5`);
     // Se o chute foir IGUAL ao número secreto
     if (numeroSecreto == chute){
-        alert(`Opa! Você acertou com apenas ${tentativas} tentativas!`);
+        alert(`| ${numeroSecreto} | Você acertou com apenas ${tentativas} tentativa(s)! Parabéns, ${nome}!`);
     } else {
         if(chute > numeroSecreto){
             alert(`${nome}, o número secreto é menor que ${chute}`);
