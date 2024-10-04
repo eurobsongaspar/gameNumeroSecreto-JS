956b7f90ac8ec47f0ecde01921a20ace6e082295
Robson Gaspar <orobsongaspar@gmail.com>
2024-10-04 04:34:49 -0300

    Modifiquei a variavel NumeroSecreto para receber números rondômicos.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let nome = prompt('Olá, seja bem-vindo ao jogo de adivinhar números. Para condinuar, digite o seu nome!');
-let numeroSecreto = 25;
+let numeroSecreto = Math.floor(Math.random() * 5) + 1; // Cria um número rondômico entre 1 e 10
+console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-   chute = prompt (`Ok, ${nome}. Escolha um número entre 1 e 30`);
+   chute = prompt (`Ok, ${nome}. Escolha um número entre 1 e 5`);
     // Se o chute foir IGUAL ao número secreto
     if (numeroSecreto == chute){
         alert(`Opa! Você acertou com apenas ${tentativas} tentativas!`);
@@ -14,7 +15,7 @@ while (chute != numeroSecreto) {
         } else {
             alert(`${nome}, o número secreto é maior que ${chute}`);
         };
-        
+
         tentativas++;
 }
 };
