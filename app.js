0a362c6d0eab3fb676f4b35648166692a502e6c7
Robson Gaspar <orobsongaspar@gmail.com>
2024-10-04 04:19:43 -0300

    Adicionei repetição e contagem de tentativas

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let nome = prompt('Olá, seja bem-vindo ao jogo de adivinhar números. Para condinuar, digite o seu nome!');
 let numeroSecreto = 25;
-let chute = prompt (`Ok, ${nome}. Escolha um número entre 1 e 30`);
-// Se o chute foir IGUAL ao número secreto
-if (numeroSecreto == chute){
-    alert('Opa! Você acertou');
-} else {
-    alert(`Você errou! O número secreto é ${numeroSecreto}`);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+   chute = prompt (`Ok, ${nome}. Escolha um número entre 1 e 30`);
+    // Se o chute foir IGUAL ao número secreto
+    if (numeroSecreto == chute){
+        alert(`Opa! Você acertou com apenas ${tentativas} tentativas!`);
+    } else {
+        if(chute > numeroSecreto){
+            alert(`${nome}, o número secreto é menor que ${chute}`);
+        } else {
+            alert(`${nome}, o número secreto é maior que ${chute}`);
+        };
+        
+        tentativas++;
+}
 };
